c4a9898308da287f030b188f3e12beffc8bec1d3
Freyr-S <60339393+Freyr-S@users.noreply.github.com>
2021-09-23 22:54:07 +0800

    fix

--- 031902640/main.py
+++ 031902640/main.py
@@ -66,6 +66,46 @@ if __name__ == '__main__':
     for i in answer.values():
         for ii in i:
             sum += 1
+    filea = open(ans_file, 'w+', encoding='utf-8')
+    filea.write('Total: {}'.format(sum)+'\n')
+    for key, value in answer.items():
+        for i in value:
+            print('Line{}: <{}> {}'.format(key, i[0], i[1]))
+            filea.write('Line{}: <{}> {}'.format(key, i[0], i[1])+'\n')
+    filea.close()
+
+if __name__ == '__main__':
+
+    word_file = sys.argv[1]
+    org_file = sys.argv[2]
+    ans_file = sys.argv[3]
+    with open(word_file, 'r', encoding='utf-8') as f:
+        words = f.read().splitlines()
+    sensitiveword = dict()
+    for word_m in words:
+        if is_english(word_m):
+            sensitiveword.update({word_m: [word_m]})
+            continue
+        sensitiveword.update(sensitive_word(word_m))
+    tree = BuildAc()
+    print(sensitiveword)
+    tree.add_word(sensitiveword)
+    tree.make_fail()
+    answer = dict()
+    filet = open(org_file, 'r+', encoding='utf-8')
+    with open(org_file, 'r', encoding='utf-8') as f:
+        txt1 = f.read().splitlines()
+    for i in range(len(txt1)):
+        pinyin_list = get_pinyin(txt1[i])
+        pinyin_txt = ''
+        for ii in pinyin_list:
+            pinyin_txt = ''.join(pinyin_list)
+        count = int(i) + 1
+        answer.update({count: tree.search(pinyin_txt.lower(), pinyin_list, list(txt1[i]))})
+    sum = 0
+    for i in answer.values():
+        for ii in i:
+            sum += 1
     filea = open(ans_file)
     filea.write('Total: {}'.format(sum)+'\n')
     for key, value in answer.items():
