66eb7e4cf706b3e5b45c6debdfa5ebecb7b61f26
Freyr-S <60339393+Freyr-S@users.noreply.github.com>
2021-09-23 21:09:59 +0800

    fix

--- 031902640/main.py
+++ 031902640/main.py
@@ -1,39 +1,56 @@
 from build_ac import *
 from sensitive_word import *
-from Simple_traditional import *
 from get_Pinyin import *
-import json
+import sys
+
+
+def is_english(check_str):
+
+    i = 0
+    for s in check_str:
+        if (u'\u0041' <= s <= u'\u005a') or (u'\u0061' <= s <= u'\u007a'):
+            i += 1
+    if i == len(check_str):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    fileo = open('C:/Users/86189/AppData/Local/Programs/Python/words (1).txt')
-    words = []
-    while 1:
-        line = fileo.readline()
-        if not line:
-            break
-        words.append(line)
-    fileo.close()
-    meaningless = [' ', '|', '/', '&', '!', '！', '@', '.', ',', ':', ';', '"', "'", '{', '}', '\\', '-', '#', '$',
-                   '￥', '*', '^', '%', '?', '？', '<', '>', '《', '》', '(', ')', '+', '~', '`']
-    sensitiveword = sensitive_word(words)
-    tree = build_ac(sensitiveword)
-    fileo = open('C:/Users/86189/AppData/Local/Programs/Python/org (1).txt', encoding='utf-8')
-    text = fileo.read()
-    simple_text = sim2tradition(text)
-    pinyin_text = get_pinyin(text)
-    answer1 = query(simple_text)
-    answer3 = dict()
+
+    word_file = sys.argv[1]
+    org_file = sys.argv[2]
+    ans_file = sys.argv[3]
+    with open(word_file, 'r', encoding='utf-8') as f:
+        words = f.read().splitlines()
+    sensitiveword = dict()
+    for word_m in words:
+        if is_english(word_m):
+            sensitiveword.update({word_m: [word_m]})
+            continue
+        sensitiveword.update(sensitive_word(word_m))
+    tree = BuildAc()
+    print(sensitiveword)
+    tree.add_word(sensitiveword)
+    tree.make_fail()
     answer = dict()
-    for i in sorted(answer1):
-        answer3[i] = answer1[i]
-    answer2 = query(pinyin_text)
-    for i in sorted(answer2):
-        answer3[i] = answer2[i]
-    for i in sorted(answer3):
-        answer[i] = answer3[i]
-        with open('C:/Users/86189/AppData/Local/Programs/Python/ans.txt', 'w') as f:
-            txt_str = json.dumps(answer, indent=0)
-            f.write(txt_str)
-            f.write('\n')
-        f.close()
+    filet = open(org_file, 'r+', encoding='utf-8')
+    with open(org_file, 'r', encoding='utf-8') as f:
+        txt1 = f.read().splitlines()
+    for i in txt1:
+        pinyin_list = get_pinyin(txt1)
+        pinyin_txt = ''
+        for i in pinyin_list:
+            pinyin_txt = ''.join(pinyin_list)
+        answer.update({list.index(i): tree.search(pinyin_txt.lower(), pinyin_list, list(txt1))})
+    sum = 0
+    for i in answer.values():
+        for ii in i:
+            sum += 1
+    filea = open(ans_file)
+    filea.write('Total: {}'.format(sum)+'\n')
+    for key, value in answer.items():
+        for i in value:
+            print('Line{}: <{}> {}'.format(key, i[0], i[1]))
+            filea.write('Line{}: <{}> {}'.format(key, i[0], i[1]))
+    filea.close()
