95c6286ce203c23092dfaa43510055430dab909f
Freyr-S <60339393+Freyr-S@users.noreply.github.com>
2021-09-23 11:12:56 +0800

    fix

--- 031902640/Simple_traditional.py
+++ 031902640/Simple_traditional.py
@@ -1,11 +1,6 @@
 from hanziconv import HanziConv
 
 
-def sim2tradition(txt):
-
-    simple = HanziConv.toSimplified(txt)
-    return simple
-
 def tradition2sim(txt):
 
     traditional = HanziConv.toTraditional(txt)
