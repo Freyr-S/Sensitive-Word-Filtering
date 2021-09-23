23d80121c95c1f7ea1b766ab9b9a71f8b05d3f2e
Freyr-S <60339393+Freyr-S@users.noreply.github.com>
2021-09-23 22:55:20 +0800

    fix

--- get_Pinyin.py
+++ get_Pinyin.py
@@ -1,8 +1,11 @@
 import pypinyin
 
 
-def get_pinyin(text):
+def get_pinyin(wordlist):
 
+    list_pinyin = list()
     gap = ''
-    piny = gap.join(pypinyin.lazy_pinyin(text))
-    return piny
+    for words in wordlist:
+        piny = gap.join(pypinyin.lazy_pinyin(words))
+        list_pinyin.append(piny)
+    return list(list_pinyin)
